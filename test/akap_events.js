7402aaf6e8bcc470cfbbe451ca6fe5a16b0e68bf
Christian Felde <cfelde@cfelde.com>
2019-12-17 14:33:49 +0000

    Ensure event tests covers both special and non-special parent cases.

--- test/akap_events.js
+++ test/akap_events.js
@@ -23,7 +23,7 @@ function sleep(ms) {
 
 contract("When testing AKAP events, it:", async accounts => {
 
-    it("should emit a Claim event of type ClaimCase.NEW when an owner claims a root node", async () => {
+    it("should emit a Claim event of type ClaimCase.NEW when an owner claims a node with special case parent", async () => {
         // This is a case 2 test with special case parent..
         let instance = await akap.deployed();
         let nodeHash = await instance.hashOf(0x0, [0x1]);
@@ -40,7 +40,7 @@ contract("When testing AKAP events, it:", async accounts => {
         await expectEvent.inLogs(receipt.logs, "Claim", args);
     });
 
-    it("should emit a Claim of type ClaimCase.RECLAIM event when owners claim existing node", async () => {
+    it("should emit a Claim of type ClaimCase.RECLAIM event when owner claim existing node with special case parent", async () => {
         // This is a case 1 test with special case parent..
         let instance = await akap.deployed();
         let nodeHash = await instance.hashOf(0x0, [0x1]);
@@ -56,10 +56,70 @@ contract("When testing AKAP events, it:", async accounts => {
         await expectEvent.inLogs(receipt.logs, "Claim", args);
     });
 
-    it("should emit a Claim of type ClaimCase.TRANSFER event when owners claim existing node", async () => {
-        // Test case 3 emits ClaimCasse.TRANSFER event
+    it("should emit a Claim event of type ClaimCase.NEW when an owner claims a node with non-special case parent", async () => {
+        // This is a case 2 test with non-special case parent..
         let instance = await akap.deployed();
         let parentHash = await instance.hashOf(0x0, [0x1]);
+        let nodeHash = await instance.hashOf(parentHash, [0x1]);
+
+        let receipt = await instance.claim(parentHash, [0x1]);
+
+        let args = {
+            sender: accounts[0].toString(),
+            nodeId: nodeHash.toString(),
+            parentId: parentHash.toString(),
+            label: "0x01",
+            claimCase: "1"
+        };
+        await expectEvent.inLogs(receipt.logs, "Claim", args);
+    });
+
+    it("should emit a Claim of type ClaimCase.RECLAIM event when owner claim existing node with non-special case parent", async () => {
+        // This is a case 1 test with non-special case parent..
+        let instance = await akap.deployed();
+        let parentHash = await instance.hashOf(0x0, [0x1]);
+        let nodeHash = await instance.hashOf(parentHash, [0x1]);
+
+        let receipt = await instance.claim(parentHash, [0x1]);
+        let args = {
+            sender: accounts[0].toString(),
+            nodeId: nodeHash.toString(),
+            parentId: parentHash.toString(),
+            label: "0x01",
+            claimCase: "0"
+        };
+        await expectEvent.inLogs(receipt.logs, "Claim", args);
+    });
+
+    it("should emit a Claim of type ClaimCase.TRANSFER event when new owner claim existing node on special case owner", async () => {
+        // Test case 3 emits ClaimCase.TRANSFER event with special case parent..
+        let instance = await akap.deployed();
+        let nodeHash = await instance.hashOf(0x0, [0x2]);
+
+        await instance.claim(0x0, [0x2]);
+        await instance.transferFrom(accounts[0], accounts[1], nodeHash);
+
+        let receipt = await instance.claim(0x0, [0x2]);
+        assert.equal(0, receipt.logs.length, "No events should be emitted, expect empty logs.");
+
+        await instance.expireNode(nodeHash, {from: accounts[1]});
+        await sleep(1000); // Wait a moment for node to expire
+        receipt = await instance.claim(0x0, [0x2]);
+
+        let args = {
+            sender: accounts[0].toString(),
+            nodeId: nodeHash.toString(),
+            parentId: "0",
+            label: "0x02",
+            claimCase: "2"
+        };
+        await expectEvent.inLogs(receipt.logs, "Claim", args);
+    });
+
+    it("should emit a Claim of type ClaimCase.TRANSFER event when new owner claim existing node on non-special case owner", async () => {
+        // Test case 3 emits ClaimCase.TRANSFER event with non-special case parent..
+        let instance = await akap.deployed();
+        let parentHash = await instance.hashOf(0x0, [0x2])
         let nodeHash = await instance.hashOf(parentHash, [0x2]);
 
         await instance.claim(parentHash, [0x2]);
@@ -82,12 +142,42 @@ contract("When testing AKAP events, it:", async accounts => {
         await expectEvent.inLogs(receipt.logs, "Claim", args);
     });
 
-    it("should emit AttributeChanged event when a node attribute is updated", async () => {
+    it("should emit AttributeChanged event when a node attribute is updated with special case parent", async () => {
         let instance = await akap.deployed();
         let nodeHash = await instance.hashOf(0x0, [0x1]);
 
-        let receipt = await instance.setSeeAlso(nodeHash, 0x1);
-        let args = {sender: accounts[0].toString(), nodeId: nodeHash.toString(), attribute: "1"}; // NodeAttribute.SEE_ALSO
+        let receipt = await instance.expireNode(nodeHash);
+        let args = {sender: accounts[0].toString(), nodeId: nodeHash.toString(), attribute: "0"}; // NodeAttribute.EXPIRY
+        await expectEvent.inLogs(receipt.logs, "AttributeChanged", args);
+
+        receipt = await instance.setSeeAlso(nodeHash, 0x1);
+        args["attribute"] = "1"; // NodeAttribute.SEE_ALSO
+        await expectEvent.inLogs(receipt.logs, "AttributeChanged", args);
+
+        receipt = await instance.setSeeAddress(nodeHash, accounts[1]);
+        args["attribute"] = "2"; // NodeAttribute.SEE_ADDRESS
+        await expectEvent.inLogs(receipt.logs, "AttributeChanged", args);
+
+        receipt = await instance.setNodeBody(nodeHash, [0x1, 0x2, 0x3]);
+        args["attribute"] = "3"; // NodeAttribute.NODE_BODY
+        await expectEvent.inLogs(receipt.logs, "AttributeChanged", args);
+
+        receipt = await instance.setTokenURI(nodeHash, "akap://abc");
+        args["attribute"] = "4"; // NodeAttribute.TOKEN_URI
+        await expectEvent.inLogs(receipt.logs, "AttributeChanged", args);
+    });
+
+    it("should emit AttributeChanged event when a node attribute is updated with non-special case parent", async () => {
+        let instance = await akap.deployed();
+        let parentHash = await instance.hashOf(0x0, [0x2]);
+        let nodeHash = await instance.hashOf(parentHash, [0x2]);
+
+        let receipt = await instance.expireNode(nodeHash);
+        let args = {sender: accounts[0].toString(), nodeId: nodeHash.toString(), attribute: "0"}; // NodeAttribute.EXPIRY
+        await expectEvent.inLogs(receipt.logs, "AttributeChanged", args);
+
+        receipt = await instance.setSeeAlso(nodeHash, 0x1);
+        args["attribute"] = "1"; // NodeAttribute.SEE_ALSO
         await expectEvent.inLogs(receipt.logs, "AttributeChanged", args);
 
         receipt = await instance.setSeeAddress(nodeHash, accounts[1]);
